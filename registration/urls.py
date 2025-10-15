ee1afd7887965fb5f6b0e3a7f189f7624a179255
EsBazza <alonzoamarojuno@gmail.com>
2025-10-15 09:26:38 +0800

    first commit

--- registration/urls.py
+++ registration/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, list_users
 
 urlpatterns = [
     path('api/register/', register_user, name='register_user'),
+    path('api/users/', list_users, name='list_users'),
 ]
